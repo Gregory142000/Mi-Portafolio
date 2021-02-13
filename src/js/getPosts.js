f2a882631bbcc564ca53344ddb0b2918f7ea32e5
Gregory Vicent <gregory142000@gmail.com>
2021-02-12 21:45:40 -0400

    Ultimos cambios por hoy

--- src/js/getPosts.js
+++ src/js/getPosts.js
@@ -5,7 +5,7 @@ export default async function getPosts() {
     $loader = document.getElementById("loader")
 
   try{
-    const myFetch = await fetch("https://gregcode.000webhostapp.com/wp-json/wp/v2/posts?_embed"),
+    const myFetch = await fetch("https://gregcode.000webhostapp.com/wp-json/wp/v2/posts?_embed&per_page=3"),
       json = await myFetch.json()
   
     console.log(json)
--- src/js/scriptJobs.js
+++
@@ -1,9 +0,0 @@
-import burgerMenu from "./burgerMenu.js"
-import scrollUp from "./scrollUp.js"
-import carousel from "../../src/carousel/src/js/carousel.js"
-
-document.addEventListener("DOMContentLoaded", () => {
-    burgerMenu()
-    scrollUp()
-    carousel()
-})
